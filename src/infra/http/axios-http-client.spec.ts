3362a205b2ec4ebf3daaa8023a33c47b86cdc38b
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-03-21 23:15:18 -0300

    refactor:  add make sut on axios-test

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -3,11 +3,16 @@ import { AxiosHttpClient } from "./axios-http-client";
 import faker from "faker";
 
 jest.mock("axios");
+
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient();
+};
 describe("AxiosHttpClient", () => {
   test("should call axios with correct url ", async () => {
     const url = faker.internet.url();
-    const sut = new AxiosHttpClient();
+    const sut = makeSut();
     await sut.post({ url });
     expect(mockedAxios).toHaveBeenCalledWith(url);
   });
