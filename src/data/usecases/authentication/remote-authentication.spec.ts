ec4fbba87b0317d0a15dd50e9e5aaf81e9acf6ac
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-03-21 22:33:26 -0300

    test:  ensure remote authentication returns an account on status code 200

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,9 @@
 import { RemoteAuthentication } from "./remote-authentication";
 import faker from "faker";
-import { mockAuthentication } from "@/domain/test/mock-authentication";
+import {
+  mockAccountModel,
+  mockAuthentication,
+} from "@/domain/test/mock-account";
 import { HttpPostClientSpy } from "@/data/test/mock-http-client";
 import InvalidCredentialsError from "@/domain/errors/invalid-credentials-error";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
@@ -84,5 +87,18 @@ describe("RemoteAuthentication", () => {
     };
     const promisse = sut.auth(mockAuthentication());
     await expect(promisse).rejects.toThrow(new UnexpectedError());
+  });
+});
+
+describe("RemoteAuthentication", () => {
+  test("Should return an AccountModel if HttpPostClient returns 200", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+    const account = await sut.auth(mockAuthentication());
+    await expect(account).toEqual(httpResult);
   });
 });
