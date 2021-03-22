f75553878383ee71b667eb1ca6f90aad43dfdc36
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-03-21 23:22:53 -0300

    refactor: create mockPostRequest func

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { AxiosHttpClient } from "./axios-http-client";
 import faker from "faker";
+import { HttpPostParams } from "@/data/protocols/http";
 
 jest.mock("axios");
 
@@ -9,11 +10,25 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
+
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement(),
+});
+
 describe("AxiosHttpClient", () => {
   test("should call axios with correct URL and method ", async () => {
-    const url = faker.internet.url();
     const sut = makeSut();
-    await sut.post({ url });
-    expect(mockedAxios.post).toHaveBeenCalledWith(url);
+    const request = mockPostRequest();
+    await sut.post(request);
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url);
   });
 });
+
+// describe("AxiosHttpClient", () => {
+//   test("should call axios with correct body", async () => {
+//     const sut = makeSut();
+//     await sut.post({ url });
+//     expect(mockedAxios.post).toHaveBeenCalledWith(url);
+//   });
+// });
