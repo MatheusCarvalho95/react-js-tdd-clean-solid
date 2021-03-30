8d2b3df0b36f244de4b11e165631699d388f891e
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-03-29 22:41:58 -0300

    test: ensure login starts with submit button disabled by default

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,11 @@ import { render } from "@testing-library/react";
 import Login from "./index";
 
 describe("Login component", () => {
-  test("", () => {
+  test("Should start with inicial state ", () => {
     const { getByTestId } = render(<Login />);
     const status = getByTestId("status-container");
     expect(status.childElementCount).toBe(0);
+    const submitButton = getByTestId("submitButton") as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
   });
 });
