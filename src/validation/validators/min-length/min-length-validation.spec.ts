396906efe5fd7359f7f05c1303d94257befe799e
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-05 00:38:26 -0300

    test: ensure min length validation returns falsy if the input is equal or greater than especified

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,9 +2,15 @@ import { InvalidFieldError } from "@/validation/errors";
 import { MinLenghtValidation } from "./min-length-validation";
 
 describe("MinLenghtValidation", () => {
-  test("should return error if value too short ", () => {
+  test("should return error if value too short", () => {
     const sut = new MinLenghtValidation("field", 5);
     const error = sut.validate("123");
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test("should return falsy if value is valid", () => {
+    const sut = new MinLenghtValidation("field", 5);
+    const error = sut.validate("12345");
+    expect(error).toBeFalsy();
   });
 });
