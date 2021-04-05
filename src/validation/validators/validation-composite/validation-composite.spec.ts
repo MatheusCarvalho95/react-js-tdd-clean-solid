ac21e2af07fbf6afcd811b5ec4da55ef60a6560d
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-05 18:28:26 -0300

    test: ensure validation composite returns the first error only

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,8 +4,10 @@ import { ValidationComposite } from "./validation-composite";
 describe("ValidationComposite", () => {
   test("should return error if any validation fails", () => {
     const fieldValidationSpy = new FieldValidationSpy("any_field");
+    fieldValidationSpy.error = new Error("any_message");
     const fieldValidationSpy2 = new FieldValidationSpy("any_field");
-    fieldValidationSpy2.error = new Error("any_message");
+    fieldValidationSpy2.error = new Error("any_message_two");
+
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2,
