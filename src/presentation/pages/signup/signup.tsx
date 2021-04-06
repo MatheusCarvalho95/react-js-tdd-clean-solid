196562a69fb96498fa0e29110ca2aef7b87e9de0
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-06 20:35:12 -0300

    test: only commit test

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -33,7 +33,7 @@ const SignUp: React.FC = () => {
             className={Styles.submitButton}
             disabled={true}
           >
-            Criar
+            Criar!
           </button>
           <Link
             to="/login"
