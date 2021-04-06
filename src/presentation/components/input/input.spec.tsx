1155b5273fe0d604fdc41c238bd6b96c1a24222e
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-05 23:34:51 -0300

    refactor: move sut to a function

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import Input from "./index";
 import Context from "./../context/form";
 
+const makeSut = (): RenderResult => {
+  return render(
+    <Context.Provider value={{ status: {} }}>
+      <Input name="field" />
+    </Context.Provider>,
+  );
+};
 describe("Input component", () => {
   test("should begin with read only", () => {
-    const { getByTestId } = render(
-      <Context.Provider value={{ status: {} }}>
-        <Input name="field" />
-      </Context.Provider>,
-    );
-    const input = getByTestId("field") as HTMLInputElement;
+    const sut = makeSut();
+    const input = sut.getByTestId("field") as HTMLInputElement;
     expect(input.readOnly).toBe(true);
   });
 });
