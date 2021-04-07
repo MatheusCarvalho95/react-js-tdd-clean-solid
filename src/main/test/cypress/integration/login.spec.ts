72bc8ab4f20918b21f55e451bc47d1a70219638d
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-07 17:10:08 -0300

    test: cypress: ensure login renders with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -3,10 +3,13 @@ describe("login", () => {
     cy.visit("login");
   });
   it("Shoud load with correct inital state", () => {
-    cy.getByTestId("email-status").should(
-      "have.attr",
-      "title",
-      "Campo obrigatório",
-    );
+    cy.getByTestId("email-status")
+      .should("have.attr", "title", "Campo obrigatório")
+      .should("contain.text", "🔴");
+    cy.getByTestId("password-status")
+      .should("have.attr", "title", "Campo obrigatório")
+      .should("contain.text", "🔴");
+    cy.getByTestId("submitButton").should("have.attr", "disabled");
+    cy.getByTestId("status-container").should("not.have.descendants");
   });
 });
