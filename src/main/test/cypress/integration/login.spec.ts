54913b3b6335dec846c7e4227cb261587ba732f8
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-07 17:04:20 -0300

    feat: add cypress helppers, add first test

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,12 @@
 describe("login", () => {
-  it("Shoud load with correct inital state", () => {
+  beforeEach(() => {
     cy.visit("login");
+  });
+  it("Shoud load with correct inital state", () => {
+    cy.getByTestId("email-status").should(
+      "have.attr",
+      "title",
+      "Campo obrigatório",
+    );
   });
 });
