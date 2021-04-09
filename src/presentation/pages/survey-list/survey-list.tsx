c9e5400d2ebdca67656b2d104e93bce9f8c40b81
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-08 22:26:54 -0300

    feat: add thumbs icon to survey status

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -20,50 +20,10 @@ const SurveyList: FC = () => {
         <ul>
           <li>
             <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>31</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Pergunta qualquer que é maior </p>
-            </div>
-            <footer>Ver Resultados</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>31</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Pergunta qualquer que é maior </p>
-            </div>
-            <footer>Ver Resultados</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>31</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Pergunta qualquer que é maior </p>
-            </div>
-            <footer>Ver Resultados</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>31</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Pergunta qualquer que é maior </p>
-            </div>
-            <footer>Ver Resultados</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
+              <div className={[Styles.iconWrap, Styles.green].join(" ")}>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC" />
+              </div>
+
               <time>
                 <span className={Styles.day}>31</span>
                 <span className={Styles.month}>10</span>
