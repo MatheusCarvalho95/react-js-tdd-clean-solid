4b8618d3bb954ec198a167c1181aa182f16b7429
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-10 14:46:34 -0300

    refactor: sut function

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import SurveyList from "./survey-list";
 
+const makeSut = (): void => {
+  render(<SurveyList />);
+};
+
 describe("Survey list component", () => {
   test("should present 4 empty list items on first render", () => {
-    render(<SurveyList />);
+    makeSut();
     const surveyList = screen.getByTestId("survey-list");
     expect(surveyList.querySelectorAll("li:empty").length).toBe(4);
   });
