dcb5b4e861de3d9ff4f63ec33b037e27f417ca58
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-10 15:43:29 -0300

    refactor: sut function

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -4,15 +4,16 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import SurveyItem from "./survey-item";
 
+const makeSut = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={survey} />);
+};
+
 describe("SurveyItem", () => {
   test("should render with correct values", () => {
     const survey = mockSurveyModel();
     survey.didAnser = true;
     survey.date = new Date("2021-04-10T00:00:00");
-    setTimeout(() => {
-      return null;
-    }, 1000);
-    render(<SurveyItem survey={survey} />);
+    makeSut(survey);
     expect(screen.getByTestId("icon")).toHaveProperty("src", IconName.thumbsUp);
     expect(screen.getByTestId("question")).toHaveTextContent(survey.question);
     expect(screen.getByTestId("day")).toHaveTextContent("10");
