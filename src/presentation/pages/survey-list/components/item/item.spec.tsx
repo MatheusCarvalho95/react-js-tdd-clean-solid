29a0c9b21fc4e969aace30201024dc86d0994b71
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-12 17:52:23 -0300

    fix: update

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -8,31 +8,31 @@ const makeSut = (survey = mockSurveyModel()): void => {
   render(<SurveyItem survey={survey} />);
 };
 
-// describe("SurveyItem", () => {
-//   test("should render with correct values", () => {
-//     const survey = mockSurveyModel();
-//     survey.didAnser = true;
-//     survey.date = new Date("2021-04-10T00:00:00");
-//     makeSut(survey);
-//     expect(screen.getByTestId("icon")).toHaveProperty("src", IconName.thumbsUp);
-//     expect(screen.getByTestId("question")).toHaveTextContent(survey.question);
-//     expect(screen.getByTestId("day")).toHaveTextContent("10");
-//     expect(screen.getByTestId("month")).toHaveTextContent("abr" || "Abr");
-//     expect(screen.getByTestId("year")).toHaveTextContent("2021");
-//   });
+describe("SurveyItem", () => {
+  test("should render with correct values", () => {
+    const survey = mockSurveyModel();
+    survey.didAnser = true;
+    survey.date = new Date("2021-04-10T00:00:00");
+    makeSut(survey);
+    expect(screen.getByTestId("icon")).toHaveProperty("src", IconName.thumbsUp);
+    expect(screen.getByTestId("question")).toHaveTextContent(survey.question);
+    expect(screen.getByTestId("day")).toHaveTextContent("10");
 
-//   test("should render with correct values", () => {
-//     const survey = mockSurveyModel();
-//     survey.didAnser = false;
-//     survey.date = new Date("2025-02-09T00:00:00");
-//     makeSut(survey);
-//     expect(screen.getByTestId("icon")).toHaveProperty(
-//       "src",
-//       IconName.thumbsDown,
-//     );
-//     expect(screen.getByTestId("question")).toHaveTextContent(survey.question);
-//     expect(screen.getByTestId("day")).toHaveTextContent("09");
-//     expect(screen.getByTestId("month")).toHaveTextContent("fev" || "Fev");
-//     expect(screen.getByTestId("year")).toHaveTextContent("2025");
-//   });
-// });
+    expect(screen.getByTestId("year")).toHaveTextContent("2021");
+  });
+
+  test("should render with correct values", () => {
+    const survey = mockSurveyModel();
+    survey.didAnser = false;
+    survey.date = new Date("2025-02-09T00:00:00");
+    makeSut(survey);
+    expect(screen.getByTestId("icon")).toHaveProperty(
+      "src",
+      IconName.thumbsDown,
+    );
+    expect(screen.getByTestId("question")).toHaveTextContent(survey.question);
+    expect(screen.getByTestId("day")).toHaveTextContent("09");
+
+    expect(screen.getByTestId("year")).toHaveTextContent("2025");
+  });
+});
