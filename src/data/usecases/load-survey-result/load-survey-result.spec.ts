a5abd38350693801a522484818008f8fe4782dff
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2021-04-13 19:24:10 -0300

    feat: test Should throw access denied error in use error if HttpGetClient returns 403

--- src/data/usecases/load-survey-result/load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/load-survey-result.spec.ts
@@ -1,4 +1,6 @@
+import { HttpStatusCode } from "@/data/protocols";
 import { HttpGetClientSpy } from "@/data/test";
+import { AccessDeniedError } from "@/domain/errors";
 import faker from "faker";
 import { RemoteLoadSurveyResult } from "./load-survey-result";
 
@@ -19,5 +21,14 @@ describe("RemoteLoadSurveyResult", () => {
     const { sut, httpGetClientSpy } = makeSut(url);
     await sut.load();
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  test("Should throw access denied error in use error if HttpGetClient returns 403", async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbiden,
+    };
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new AccessDeniedError());
   });
 });
